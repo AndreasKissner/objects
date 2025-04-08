2398916b757e78f853f9d299954ce76e11609919
Andreas Kissner <andikiss500@gmail.com>
2025-04-08 20:45:56 +0200

    For Schleife Added

--- script.js
+++ script.js
@@ -2,5 +2,15 @@ let bible = {
     "1chapter" : "Moses",
     "2chapter" : "2.Moses",
     "3chapter" : "3.Mose",
-    "4chapter" : "4.Mose",
+    "4chapter" : "4.Mose"
+};
+
+function bibleTest() {
+    let content = "";
+    for (let key in bible) {
+        content += `<li>${key}: ${bible[key]}</li>`;
+    }
+    document.getElementById("content").innerHTML = content;
 }
+
+bibleTest();
